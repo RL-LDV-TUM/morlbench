59579560b16787701d5726d9566fd3f6d354d635
Dominik <meyerd@mytum.de>
2012-11-19 19:19:45 +0100

    - reinforcement learning newcomb agent with SARSA

--- rlcomb/agents.py
+++ rlcomb/agents.py
@@ -7,6 +7,7 @@ Created on Nov 19, 2012
 from helpers import virtualFunction, SaveableObject
 
 import numpy as np
+import random
 import logging as log
 
 log.basicConfig(level=log.DEBUG)
@@ -34,19 +35,6 @@ class NewcombAgent(SaveableObject):
 
     def __str__(self):
         return self.__class__.__name__
-
-    def interact(self, n=1):
-        '''
-        Interact n times with the problem and return
-        the array of payouts.
-        '''
-        log.info('Playing %i interactions ... ' % (n))
-        payouts = []
-        for t in xrange(n):
-            a = self._decide(t)
-            p = self.newcomb_problem.play(a)
-            payouts.append(p)
-        return np.array(payouts)
 
     def _decide(self, t):
         '''
@@ -86,3 +74,67 @@ class TwoBoxNewcombAgent(NewcombAgent):
 
     def _decide(self, t):
         return 1
+
+
+class RLNewcombAgent(NewcombAgent):
+    '''
+    A Newcomb agent, that uses RL to decide which
+    boxing to do next.
+    '''
+
+    def __init__(self, problem, alpha=0.3, gamma=1.0, epsilon=1.0):
+        '''
+        Initialize the Reinforcement Learning Newcomb
+        Agent with the probleme description and alpha,
+        the learning rate.
+
+        Parameters
+        ----------
+        problem: A Newcomb problem
+        alpha: real, the learning rate in each
+            SARSA update step
+        gamma: real, [0, 1) RL discount factor
+        epsilon: real, [0, 1] the epsilon factor for
+            the epsilon greedy action selection strategy
+        '''
+        super(RLNewcombAgent, self).__init__(problem)
+
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        # the Q function is only one dimensional, since
+        # we can only be in one state and choose from
+        # two actions in the newcomb problem.
+        self.Q = np.zeros(len(self.newcomb_problem.actions))
+
+    def interact(self, n=1):
+        '''
+        Interact n times with the problem and return
+        the array of payouts.
+        '''
+        log.info('Playing %i interactions ... ' % (n))
+        payouts = []
+        last_payout = 0
+        last_action = 0
+        for t in xrange(n):
+            action = self._decide(t)
+            payout = self.newcomb_problem.play(action)
+            self._learn_sarsa(t, last_action,
+                              last_payout, action, payout)
+            payouts.append(payout)
+            last_action = action
+            last_payout = payout
+            log.debug(' step %05i: action: %i, payout: %i' % \
+                      (t, action, payout))
+        return np.array(payouts)
+
+    def _learn_sarsa(self, t, last_action,
+                     last_payout, action, payout):
+        self.Q[last_action] += self.alpha * \
+            (payout + self.gamma * self.Q[action] - self.Q[last_action])
+        log.debug(' Q: %s' % (str(self.Q)))
+
+    def _decide(self, t):
+        if random.random() < self.epsilon:
+            return self.Q.argmax()
+        return random.randint(0, len(self.newcomb_problem.actions) - 1)
--- rlcomb/rlcomb.py
+++ rlcomb/rlcomb.py
@@ -10,15 +10,15 @@ import numpy as np
 log.basicConfig(level=log.DEBUG)
 
 from problems import Newcomb
-from agents import OneBoxNewcombAgent, TwoBoxNewcombAgent
+from agents import OneBoxNewcombAgent, TwoBoxNewcombAgent, RLNewcombAgent
 
 
 if __name__ == '__main__':
     problem = Newcomb(predictor_accuracy=0.99,
                       payouts=np.array([[1000000, 0], [1001000, 1000]]))
-    agent = TwoBoxNewcombAgent(problem)
+    agent = RLNewcombAgent(problem, alpha=0.1, gamma=0.9, epsilon=0.9)
 
-    interactions = 100000
+    interactions = 10000
 
     log.info('Playing ...')
     log.info('%s' % (str(agent)))

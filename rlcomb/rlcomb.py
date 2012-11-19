e7cfbac54e224b105eb01333f018bcd33a4b68a1
Dominik <meyerd@mytum.de>
2012-11-19 14:08:46 +0100

    - initial version of the Newcomb problem implementation skeleton

---
+++ rlcomb/rlcomb.py
@@ -0,0 +1,25 @@
+'''
+Created on Nov 19, 2012
+
+@author: Dominik Meyer <meyerd@mytum.de>
+'''
+
+import numpy as np
+import logging as log
+
+
+class Newcomb(object):
+    """
+    This class represents a Newcomb problem.
+    All the parameters should be set up on object
+    creation. Then the Newcomb problem can be played
+    either once or iteratively. By setting the values
+    for payouts specifically this can also be used
+    to model the Prisoner's Dilemma.
+    """
+
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    pass

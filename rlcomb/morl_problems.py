b9b9d59901e161ceba7351f0f38b2c2d7dce631e
Johannes <johannes.feldmaier@tum.de>
2016-02-11 22:59:04 +0100

    moving around and receiving reward done! Bug: Moving into ground is possible -> strange behavior of "+=" in line 137

--- rlcomb/morl_problems.py
+++ rlcomb/morl_problems.py
@@ -7,102 +7,163 @@ Created on Nov 19, 2012
 from helpers import SaveableObject
 
 import numpy as np
+import matplotlib.pyplot as plt
 import random
 
 
-class Newcomb(SaveableObject):
+class Deepsea(SaveableObject):
     """
-    This class represents a Newcomb problem.
+    This class represents a Deepsea problem.
     All the parameters should be set up on object
-    creation. Then the Newcomb problem can be played
-    either once or iteratively.
+    creation. Then the Deepsea problem can be used
+    iteratively by calling "action".
     """
 
-    def __init__(self, predictor_accuracy=0.99,
-                 payouts=np.array([[1000000, 0], [1001000, 1000]]),
-                 actions=["one box", "two box"]):
+    def __init__(self, map=[], actions=["up", "down", "right", "left"], state=0):
         '''
-        Initialize the Newcomb problem.
+        Initialize the Deepsea problem.
 
         Parameters
         ----------
-        predictor_accuracy: real [0,1], How good the one/two box
-            predictor is predicting which one you will
-            choose and therefore this is at the same
-            time the probability of placement of
-            the million dollar conditioned on the
-            chosen boxing.
-        payouts: array, size (2,2). Where the payouts
-            are denoted if one/two boxing was predicted
-            and one/two boxing was chosen. The first
-            row denotes the payouts for chosen one boxing,
-            the second for two boxing. The first column
-            represents the values for predicted one
-            boxing, the second for predicted two boxing.
-        actions: The name of the two actions (one/two
-            boxing, used for debugging output purposes
-            only.
+        map: array, Map of the deepsea landscape. Entries represent
+            rewards. Invalid states get a value of "-100" (e.g. walls, ground).
+            Positive values correspond to treasures.
+        actions: The name of the actions: Here the directions the
+            submarine can move - left, right, up, down.
         '''
 
-        super(Newcomb, self).__init__(
-            ['predictor_accuracy, payouts, actions'])
+        super(Deepsea, self).__init__(
+            ['energy, time, actions, map'])
 
-        self.predictor_accuracy = predictor_accuracy
-        self.payouts = payouts
-        self.actions = actions
+        self._time = 0
 
-    def __str__(self):
-        return 'Newcomb problem with\n actions:\n%s\n\
- predictor_accuracy:\n%03f\n\
- payouts:\n%s' % (str(self.actions), self.predictor_accuracy, self.payouts)
+        self._state = state
+        self._last_state = state
 
-    def __invert_action(self, action):
-        '''
-        Invert the action, if the predictor will
-        predict wrong.
-        '''
-        if action == 0:
-            return 1
-        return 0
+        # Default Map as used in general MORL papers
+        self._map = np.zeros((11,10))
+        self._map[2:11,0] = -100
+        self._map[3:11,1] = -100
+        self._map[4:11,2] = -100
+        self._map[5:11,3:6] = -100
+        self._map[8:11,6:8] = -100
+        self._map[10,8] = -100
+        # Rewards of the default map
+        self._map[1,0] = 1
+        self._map[2,1] = 2
+        self._map[3,2] = 3
+        self._map[4,3] = 5
+        self._map[4,4] = 8
+        self._map[4,5] = 16
+        self._map[7,6] = 24
+        self._map[7,7] = 50
+        self._map[9,8] = 74
+        self._map[10,9] = 124
+
+        self._flat_map = np.ravel(self._map, order='C') #flat map with Fortran-style order (column-first)
+
+        self._position = self.get_position(state)
+        self._last_position = self._position
+
+        #self._predictor_accuracy = predictor_accuracy
+        #self._payouts = payouts
+        self._actions = actions
+
+    def get_index(self, position):
+        if self.in_map(position):
+            #raise IndexError("Position out of bounds: {}".format(position))
+            #print np.ravel_multi_index(position, self._map.shape)
+            return np.ravel_multi_index(position, self._map.shape)
+        else:
+            print('Error: Position out of map!')
+            return -1
+
+
+    def get_position(self, index):
+        if (index < (self._map.shape[0] * self._map.shape[1])):
+            return np.unravel_index(index, self._map.shape)
+        else:
+            print('Error: Index out of list!')
+            return -1
+
+    def in_map(self,position):
+        return not((position[0] < 0) or (position[0] > self._map.shape[0]-1) or (position[1] < 0) or (position[1] > self._map.shape[1]-1))
+
+    def print_map(self):
+        plt.imshow(self._map, interpolation='none')
+
+ #    def __str__(self):
+ #        return 'Newcomb problem with\n actions:\n%s\n\
+ # predictor_accuracy:\n%03f\n\
+ # payouts:\n%s' % (str(self.actions), self.predictor_accuracy, self.payouts)
+
+    # def __invert_action(self, action):
+    #     '''
+    #     Invert the action, if the predictor will
+    #     predict wrong.
+    #     '''
+    #     if action == 0:
+    #         return 1
+    #     return 0
 
     def play(self, action):
         '''
-        Play one round of the Newcomb problem. A iterated
-        Newcomb problem can be obtained by simply iteratively
-        playing the same problem over and over again.
+        Perform an action with the submarine
+        and receive reward (or not).
 
         Parameters
         ----------
         action: integer, Which action will be chosen
-            the agent. (0: one box, 1: two box).
+            the agent. (0: left, 1: right, 2: up, 3: down).
 
         Returns
         -------
-        payout: payout of the current round.
+        reward: reward of the current state.
         '''
-        # decide whether the predictor is correct this
-        # round
-        # TODO: if the predictor is not correct does it
-        #       predict a wrong answer or just a random one
 
-        predictor_action = action
-        if random.random() > self.predictor_accuracy:
-            predictor_action = self.__invert_action(predictor_action)
+        map_actions = {
+            'up': np.array([-1, 0]),
+            'down': np.array([1, 0]),
+            'right': np.array([0, 1]),
+            'left': np.array([0, -1]),
+            }
 
-        return self.payouts[action, predictor_action]
+        self._time += 1
 
-class moProb(SaveableObject):
-    def __init__(selfs):
-        pass
+        self._last_position = self._position
+        print('Position before: ' + str(self._position) + ' moving ' + self._actions[action])
+        #self._position += map_actions[self._actions[action]] #try new position
+        if self.in_map(self._position + map_actions[self._actions[action]]):
+            self._position += map_actions[self._actions[action]]
+            if self._flat_map[self.get_index(self._position)] < 0:
+                self._position = self._last_position
+                reward = self._flat_map[self.get_index(self._position)]
+                print('Ground touched!')
+            else:
+                reward = self._flat_map[self.get_index(self._position)]
+        else:
+            print('Move not allowed!')
+            reward = 0
 
-    def __str__(self):
-        return 'Multi objective problem with %i actions and %i objectives.\n' % (self.actions, self.nbObjectives)
+        print(self._position)
+
+        self._last_state = self._state
+        self._state = self.get_index(self._position)
+
+        # predictor_action = action
+        # if random.random() > self.predictor_accuracy:
+        #     predictor_action = self.__invert_action(predictor_action)
+
+        return reward
 
 
-class deepsea(moProb):
-    def __init__(self):
-        self.actions = 4
-        self.nbObjectives = 2
+class DeepseaEnergy(Deepsea):
+    def __init__(self, energy = 200):
+        '''
+        energy: integer > 0, Amount of energy the
+            the submarines battery is loaded.
+        '''
+
 
     def __str__(self):
         return self.__class__.__name__
@@ -111,133 +172,3 @@ class deepsea(moProb):
         pass
 
 
-class RandomNewcomb(Newcomb):
-    '''
-    A newcomb problem which doesn't decide always the
-    opposite, but instead a random boxing.
-    '''
-
-    def __invert_action(self, action):
-        '''
-        Play random action instead of
-        the opposite of the optimal
-        '''
-        return random.randint(0, len(self.actions) - 1)
-
-
-class PrisonersDilemma(SaveableObject):
-    '''
-    The classical prisoners dilemma (PD) with a default
-    payout matrix of
-
-                        prisoner 2
-
-                          C     D
-
-    prisoner 1      C   (R,R) (S,T)
-                    D   (T,S) (P,P)
-
-
-        with T > P > R > S and R > (T + S) / 2.
-    '''
-
-    def __init__(self, T=5.0, R=3.0, P=1.0, S=0.0):
-        '''
-        Initialize the Prisoner's Dilemma.
-
-        Parameters
-        ----------
-        T: float
-            Temptation to defect.
-        R: float
-            Reward for mutual cooperation.
-        P: float
-            Punishment for mutual defection.
-        S: float
-            Sucker's payoff.
-
-        default values:
-            T = 5, R = 3, P = 1, S = 0
-        '''
-
-        super(PrisonersDilemma, self).__init__(
-                ['T', 'R', 'P', 'S', 'payouts', 'actions'])
-
-        self.T = T
-        self.R = R
-        self.P = P
-        self.S = S
-        self.actions = ['cooperate', 'defect']
-        self.payouts = [[(R, R), (S, T)], [(T, S), (P, P)]]
-
-    def __str__(self):
-        return self.__class__.__name__ + \
-            "(T=%f, R=%f, P=%f, S=%f)" % (self.T,
-                                          self.R,
-                                          self.P,
-                                          self.S)
-
-    def play(self, action1, action2):
-        '''
-        Play the PD.
-
-        Parameters
-        ----------
-        action1: int
-            Action for prisoner 1.
-        action2: int
-            Action for prisoner 2.
-
-        Returns
-        -------
-        (payout1, payout2): float 2-tuple
-            Containing the payouts for prisoner 1 and 2 respectively.
-        '''
-        return self.payouts[action1][action2]
-
-
-class ProbabilisticPrisonersDilemma(PrisonersDilemma):
-    '''
-    A modification of the Prisoner's Dilemma, where the action
-    of the second player is not chosen by input, but randomly
-    according to some cooperation probability.
-    '''
-
-    def __init__(self, T=5.0, R=3.0, P=1.0, S=0.0, coop_p=0.5):
-        '''
-        Initialize the probabilistic PD.
-
-        Parameters
-        ----------
-        coop_p: float [0,1]
-            Set the cooperation probability.
-
-        Other parameters, see superclass.
-        '''
-
-        super(ProbabilisticPrisonersDilemma, self).__init__(T=T, R=R, P=P, S=S)
-
-        self.coop_p = coop_p
-
-    def __str__(self):
-        return "ProbabilisticPrisonersDilemma(T=%.2f, R=%.2f, P=%.2f, S=%.2f, \
-coop_p=%f)" % (self.T, self.R, self.P, self.S, self.coop_p)
-
-    def play(self, action):
-        '''
-        Play the probabilistic PD. 
-
-        Parameters
-        ----------
-        action: int
-            Action for prisoner 1.
-
-        Returns
-        -------
-        (payout1, payout2): float 2-tuple
-            Containing the payouts for prisoner 1 and 2 respectively.
-        '''
-        p2action = 1
-        if random.random() <= self.coop_p:
-            p2action = 0
-        return self.payouts[action][p2action]

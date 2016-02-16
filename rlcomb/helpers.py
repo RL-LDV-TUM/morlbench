8824435861b9cc7eb35c55fe689f12697b663f59
Dominik <meyerd@mytum.de>
2016-02-16 17:10:44 +0100

    add probability matrix sanity check functions

--- rlcomb/helpers.py
+++ rlcomb/helpers.py
@@ -1,38 +1,53 @@
-'''
+"""
 Created on Sep 14, 2012
 
 @author: Dominik Meyer <meyerd@mytum.de>
-'''
+"""
 
-'''
+"""
 This module contains miscellaneous helper
 functions that didn't fit in any other place.
-'''
+"""
 
 import cPickle as pickle
 from os.path import isfile
+import numpy as np
 
 
 def virtualFunction():
     raise RuntimeError('Virtual function not implemented.')
 
 
+def assureProbabilityMatrix(P):
+    """
+    Checks if the matrix P is a valid probability transition matrix.
+    This means it has to contain transitions from state i to state j
+    in the (i, j)-th entry (i-th column, j-th row).
+
+    :param P: Probability transition matrix
+    """
+    if P.shape[0] != P.shape[1]:
+        raise RuntimeError("Probability matrix check failed: Matrix is not square.")
+    psum = P.sum(axis=1)
+    if np.abs(psum - 1.0).any() > np.finfo(P.dtype).eps:
+        raise RuntimeError("Probability matrix check failed: Columns don't add up to one.")
+
 class SaveableObject(object):
-    '''
+    """
     This should be a generic class for custom object
     storage, where you may not want to store all
     data associated with it, but only specific parts
     of the __dict__ of the object.
-    '''
+    """
 
     def __init__(self, keys, **kwargs):
-        '''
+        """
         Parameters
         ----------
-        keys: a list of __dict__ entries that
+        :param keys: a list of __dict__ entries that
             should be stored, when the object
             is pickled.
-        '''
+        """
         self._saveable_keys_ = keys
 
 #    def __getstate__(self):
@@ -48,9 +63,12 @@ class SaveableObject(object):
 #            self.__dict__[k] = v
 
     def save(self, filename, overwrite=False):
-        '''
+        """
         Save the object to disk using cPickle.
-        '''
+
+        :param filename: Filename where to save the serialized object.
+        :param overwrite: Overwrite existing files.
+        """
         if isfile(filename) and not overwrite:
             raise RuntimeError('file %s already exists.' % (filename))
         f = open(filename, 'wb')
@@ -64,9 +82,11 @@ class SaveableObject(object):
         f.close()
 
     def load(self, filename):
-        '''
+        """
         Load pickled object from file.
-        '''
+
+        :param filename: Filename where to load the object from.
+        """
         if not isfile(filename):
             raise RuntimeError('file %s does not exist' % (filename))
         f = open(filename, 'rb')

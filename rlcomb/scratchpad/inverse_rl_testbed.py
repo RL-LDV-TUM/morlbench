6cf1957ce9241d8efe826c7ede5987cec8f7a98b
Johannes <johannes.feldmaier@tum.de>
2016-02-29 10:20:37 +0100

    Deep sea scene restored.

--- rlcomb/scratchpad/inverse_rl_testbed.py
+++ rlcomb/scratchpad/inverse_rl_testbed.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     # i_morl = InverseMORL(problem, policy)
     # scalarization_weights = i_morl.solve()
     # scalarization_weights = np.array([0.153, 0.847])
-    scalarization_weights = np.array([0.2, 0.8])
+    scalarization_weights = np.array([0.8, 0.2])
 
     eps = 0.95
     alfa = 0.3
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     agent = QMorlAgent(problem, scalarization_weights, alpha=alfa, epsilon=eps)
     # agent = PreScalarizedQMorlAgent(problem, scalarization_weights, alpha=alfa, epsilon=eps)
     # agent = SARSALambdaMorlAgent(problem, scalarization_weights, alpha=alfa, epsilon=eps, lmbda=0.9)
-    interactions = 500
+    interactions = 100000
     payouts, moves, states = morl_interact_multiple(agent, problem, interactions, max_episode_length=150)
 
     plt.ion()

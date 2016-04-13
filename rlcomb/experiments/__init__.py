d671ac3aa36e06830bb9f9a770d03811ef53ea48
ga35voz <ga35voz@mytum.de>
2016-04-13 14:42:33 +0200

    remove old data

--- rlcomb/experiments/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Created on Nov 19, 2012
-
-@author: Dominik Meyer <meyerd@mytum.de>
-"""
--- rlcomb/scratchpad/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Created on Feb 16, 2016
-
-@author: Dominik Meyer <meyerd@mytum.de>
-"""

6d867641d2ddd64e54519ca825d06f2bfa37dc7e
Simon Wölzmüller <ga35voz@mytum.de>
2016-08-11 22:06:51 +0200

    add list compressing, structure and hv calc

--- morlbench/helpers.py
+++ morlbench/helpers.py
@@ -104,6 +104,14 @@ class SaveableObject(object):
         self.__dict__.update(state)
 
 
+def remove_duplicates(array):
+
+    array = [tuple(element) for element in array]
+    removed = set(array)
+    removed_array = [elem for elem in removed]
+    return removed_array
+
+
 class HyperVolumeCalculator:
     """
     this class computes hypervolumes
@@ -118,6 +126,7 @@ class HyperVolumeCalculator:
         """
         self.ref_point = ref_point
         self.list = []
+        self.list_compressing = False
 
     def extract_front(self, given_points):
         """
@@ -147,10 +156,10 @@ class HyperVolumeCalculator:
         # sort first dimension
         points = np.vstack((points))
         points = sorted(points, key=lambda y: y[0])[::-1]
-        # add the first dimension(yet sorted)
+        # add the first point
         pareto_front = []
         pareto_front.append(points[0])
-        # test other dimension in pairs
+        # test other points in missing dimension
         for pair in points[1:]:
             # append point to pareto front if it dominates other points
             if pair[1] >= pareto_front[-1][1]:
@@ -171,30 +180,32 @@ class HyperVolumeCalculator:
             pareto_front = points[0:1][:]
             # test next point
             for point in points[1:][:]:
+                # if all components of candidate are greater than the corresponding  components of the first element
                 if sum([point[x] >= pareto_front[-1][x] for x in xrange(len(point))]) == len(point):
+                    # add that element to the front
                     pareto_front = np.concatenate((pareto_front, [point]))
             return pareto_front
         else:
             return 0
 
+    def dominates(self, p, q, k=None):
+        # finds out if some point dominates another
+        if k is None:
+            k = len(p)
+        d = True
+        # test every dimension
+        while d and k < len(p):
+            d = not (q[k] > p[k])
+            k += 1
+        return d
+
     def compute_hv(self, point_set):
         """
         computes hypervolume. at first it searches pareto front and selects points that are dominating refpoint
-        this function is a slightly adapted version of inspyred lib's hypervolume calculator
-        :param point_set: pareto set of points
-        :return: volume the pareto set expands
+        this function is a slightly adapted version of While et al HSO - ALgorithm, a hypervolume calculator:
+
         """
 
-        def dominates(p, q, k=None):
-            # finds out if some point dominates another
-            if k is None:
-                k = len(p)
-            d = True
-            # test every dimension
-            while d and k < len(p):
-                d = not (q[k] > p[k])
-                k += 1
-            return d
 
         def insert(p, k, pl):
             ql = []
@@ -203,7 +214,7 @@ class HyperVolumeCalculator:
                 pl = pl[1:]
             ql.append(p)
             while pl:
-                if not dominates(p, pl[0], k):
+                if not self.dominates(p, pl[0], k):
                     ql.append(pl[0])
                 pl = pl[1:]
             return ql
@@ -233,11 +244,14 @@ class HyperVolumeCalculator:
         if reference_point:
             for point in relevant_points:
                 # only consider points that dominate the reference point
-                if dominates(point, reference_point, None):
+                if self.dominates(point, reference_point, None):
                     np.append(relevant_points, point)
 
         # compute the hypervolume
-        ps = relevant_points
+        if self.list_compressing:
+            ps = remove_duplicates(relevant_points)
+        else:
+            ps = relevant_points
         # store reference point
         ref = self.ref_point
         # find out dimension
@@ -249,7 +263,7 @@ class HyperVolumeCalculator:
         pl = np.vstack(ps[:])
         pl = sorted(pl, key=lambda q: q[0])[::-1]
         s = [(1, pl)]
-        for k in range(n - 1):
+        for k in xrange(n - 1):
             s_prime = []
             for x, ql in s:
                 # slice the volume in little volumes
@@ -274,3 +288,10 @@ def fn_timer(function):
                )
         return result
     return function_timer
+
+
+
+
+
+
+

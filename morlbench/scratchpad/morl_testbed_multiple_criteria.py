bf92a9b1e9c89ebd42c3adb9ff458a09d73f1b00
Simon Wölzmüller <ga35voz@mytum.de>
2016-08-11 21:58:53 +0200

    add some plot for average reward

--- morlbench/scratchpad/morl_testbed_multiple_criteria.py
+++ morlbench/scratchpad/morl_testbed_multiple_criteria.py
@@ -7,8 +7,8 @@ Created on Jun 07, 2016
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from morlbench.morl_agents_multiple_crit import MultipleCriteriaH, MultipleCriteriaR
-from morlbench.morl_problems import MORLBuridansAssProblem, MOPuddleworldProblem, MORLGridworld, MORLMountainCar, MORLResourceGatheringProblem, Deepsea
+from morlbench.morl_agents_multiple_criteria import MultipleCriteriaH, MultipleCriteriaR
+from morlbench.morl_problems import MORLBuridansAssProblem, MOPuddleworldProblem, MORLGridworld, MountainCarAcceleration, MORLResourceGatheringProblem, Deepsea
 from morlbench.morl_policies import PolicyFromAgent
 from morlbench.plot_heatmap import policy_heat_plot, policy_plot2, policy_plot
 from morlbench.helpers import HyperVolumeCalculator
@@ -22,35 +22,48 @@ if __name__ == '__main__':
     # how many vectors do you want to train before?
     n_vectors = 100
     # which problem do you want to experiment on?
-    problem = MORLMountainCar()
+    problem = MORLGridworld()
     # this is the threshold that the difference of the weighted average reward of the untrained and trained policy
     # should top, to let the new trained policy be a 'better policy'
-    delta = 0.1
+    deltah = 0.2
+    deltar = 0.3
     # epsilon = propability that the agent choses a greedy action instead of a random action
-    epsilon = 0.2
+    epsilon = 0.9
     # learning rate
-    alfa = 0.1
-    beta = 0.2
+    alfar = 0.2
+    alfah = 0.65
+    betar = 0.01
     # how many interactions should the action train per weight?
     interactions = 100000
     # how many episodes in one interactions should be taken? (if the problem gets in a terminal state, it will be
     # interrupted anyway (episodes = steps in the environment = actions)
     max_per_interaction = 200
     # count of final weighted average reward that don't differ from the last ones to interrupt and signify converge
-    converging_criterium = 20
+    converging_criterium = 25
     ref = [-1.0, ]*problem.reward_dimension
     # we want to evaluate both policy set with hypervolume indicator
     hv_calculator = HyperVolumeCalculator(ref)
     # create agent
     # agent = MultipleCriteriaH(problem, n_vectors, delta, epsilon, alfa, interactions, max_per_interaction,
     #                          converging_criterium)
-    agent = MultipleCriteriaR(problem, n_vectors, 0.5, epsilon, 0.1, beta,  interactions, max_per_interaction,
+    agent = MultipleCriteriaR(problem, n_vectors, deltar, epsilon, alfar, betar,  interactions, max_per_interaction,
                               converging_criterium)
-    agent2 = MultipleCriteriaH(problem, n_vectors, 0.01, epsilon, 0.4,  interactions, max_per_interaction,
+    agent2 = MultipleCriteriaH(problem, n_vectors, deltah, epsilon, alfah,  interactions, max_per_interaction,
                               converging_criterium)
     # start the training
     agent.weight_training()
     agent2.weight_training()
+    hvs = []
+    rho = [i for i in agent.rhos.values()]
+    pf = []
+    for i in xrange(len(rho)):
+        pf.append(rho[i])
+        hvs.append(hv_calculator.compute_hv(pf))
+    x = np.arange(len(hvs))
+    plt.figure()
+    plt.plot(x, hvs)
+    plt.xlabel('policy')
+    plt.ylabel('weighted average reward')
     print "R-Learning found %i policy/-ies, H-Learning found %i policy/-ies" % (len(agent.pareto), len(agent2.pareto))
     plt.figure()
     if problem.reward_dimension == 2:
@@ -83,7 +96,7 @@ if __name__ == '__main__':
     print 'R:'+str(hv_calculator.compute_hv(agent.pareto))
     print 'H:'+str(hv_calculator.compute_hv(agent2.pareto))
     # now you can choose a specific weight and train on it
-    specific_weight = [0.0, 0.0, 1.0]
+    specific_weight = [1.0, 0.0, 0.0]
     mean_count = np.mean(agent.interactions_per_weight)
     mean_count2 = np.mean(agent2.interactions_per_weight)
     agent.train_one_weight(specific_weight)
@@ -97,8 +110,8 @@ if __name__ == '__main__':
 
     agent.plot_interaction_rhos(specific_weight)
     agent2.plot_interaction_rhos(specific_weight)
-    # policy = PolicyFromAgent(agent.problem, agent, mode='greedy')
-    # policy_plot2(problem, policy)
+    policy = PolicyFromAgent(agent2.problem, agent2, mode='greedy')
+    policy_plot2(problem, policy)
     # print policy._pi
 
 

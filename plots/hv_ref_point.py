a125ce6966f6d86075a0fb8536247b96cca73888
Simon Wölzmüller <ga35voz@mytum.de>
2016-08-24 12:24:23 +0200

    hypervolume variation and its results

---
+++ plots/hv_ref_point.py
@@ -0,0 +1,32 @@
+from morlbench.morl_problems import MORLResourceGatheringProblem, MountainCarTime, MORLGridworld, MORLBuridansAss1DProblem, \
+        Deepsea, MOPuddleworldProblem
+from morlbench.morl_agents import MORLScalarizingAgent, MORLHVBAgent
+from morlbench.experiment_helpers import morl_interact_multiple_episodic
+from morlbench.morl_policies import PolicyFromAgent
+from morlbench.plot_heatmap import policy_heat_plot, policy_plot2
+from morlbench.plotting_stuff import plot_hypervolume
+
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import logging as log
+
+if __name__ == '__main__':
+    # create Problem
+    problem = MORLGridworld()
+    # learning rate
+    alfacheb = 0.11
+    eps = 0.9
+    ref_points = [[-1.0, -100.0, -1.0], [-100.0, -1.0, -1.0], [-1.0, -1.0, -100.0]]
+    agents = []
+    scalarization_weights = [0.0, 0.0]
+    interactions = 2000
+    log.info('Started reference point experiment')
+    for ref_p in xrange(len(ref_points)):
+        agents.append(MORLHVBAgent(problem, alfacheb, eps, ref_points[ref_p], scalarization_weights))
+
+        payouts, moves, states = morl_interact_multiple_episodic(agents[ref_p], problem, interactions)
+        policy = PolicyFromAgent(problem, agents[ref_p], mode='greedy')
+        policy_heat_plot(problem, policy, states)
+    plot_hypervolume(agents, problem, name='reference point')
+

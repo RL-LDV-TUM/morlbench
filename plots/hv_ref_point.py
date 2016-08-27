a3fc29ea9f3fb3d6c598933d56f0bf1017f35175
Simon Wölzmüller <ga35voz@mytum.de>
2016-08-27 14:56:50 +0200

    add average payout as output

--- plots/hv_ref_point.py
+++ plots/hv_ref_point.py
@@ -17,16 +17,23 @@ if __name__ == '__main__':
     # learning rate
     alfacheb = 0.11
     eps = 0.9
-    ref_points = [[-1.0, -100.0, -1.0], [-100.0, -1.0, -1.0], [-1.0, -1.0, -100.0]]
+    ref_points = [[-0.1, -100.0, -0.1], [-100.0, -0.1, -0.1], [-0.1, -0.1, -100.0]]
     agents = []
     scalarization_weights = [0.0, 0.0]
-    interactions = 2000
+    interactions = 8000
     log.info('Started reference point experiment')
+    payoutslist = []
     for ref_p in xrange(len(ref_points)):
         agents.append(MORLHVBAgent(problem, alfacheb, eps, ref_points[ref_p], scalarization_weights))
 
         payouts, moves, states = morl_interact_multiple_episodic(agents[ref_p], problem, interactions)
+        payoutslist.append(payouts)
         policy = PolicyFromAgent(problem, agents[ref_p], mode='greedy')
-        policy_heat_plot(problem, policy, states)
+        # policy_heat_plot(problem, policy, states)
+
     plot_hypervolume(agents, problem, name='reference point')
+    print 'final average reward' + str(sum(payoutslist[0]))
+    print 'final average reward' + str(sum(payoutslist[1]))
+    print 'final average reward' + str(sum(payoutslist[2]))
+
 

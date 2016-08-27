e53a66bfa1b31a90d469be3f8f6cd0d406815252
Simon Wölzmüller <ga35voz@mytum.de>
2016-08-27 15:00:37 +0200

    adapt on bugfix

--- morlbench/scratchpad/morl_testbed_resource_gathering.py
+++ morlbench/scratchpad/morl_testbed_resource_gathering.py
@@ -39,33 +39,35 @@ if __name__ == '__main__':
     # scalarization_weights = np.array([0.0, 1.0])
     # scalarization_weights = np.array([0.9, 0.1])
 
-    eps = 0.8
+    eps = 0.9
     alfa = 0.4
-    runs = 2
+    runs = 1
     interactions = 100
+    max_steps = 100
+    tau = 3.0
 
-
-    agent = MORLScalarizingAgent(problem, scalarization_weights, alfa, eps, 4.0, [-1.0, -1.0, -1.0])
-
+    agent = MORLScalarizingAgent(problem, scalarization_weights, alpha=alfa, epsilon=eps, tau=tau, lmbda=1.0,
+                                 ref_point=[-1.0, -1.0, -1.0])
 
 
     # payouts, moves, states = morl_interact_multiple_average_episodic(agent, problem, runs=runs, interactions=interactions, max_episode_length=150)
-    payouts, moves, states = morl_interact_multiple_episodic(agent, problem, interactions=interactions, max_episode_length=150)
+    payouts, moves, states = morl_interact_multiple_episodic(agent, problem, interactions=interactions)
+    log.info('Average Payout: %s' % (str(payouts.mean(axis=0))))
 
-    learned_policy = PolicyFromAgent(problem, agent, mode='gibbs')
+    learned_policy = PolicyFromAgent(problem, agent, mode='greedy')
     # learned_policy = PolicyFromAgent(problem, agent, mode='greedy')
 
     # filename = 'figure_' + time.strftime("%Y%m%d-%H%M%S")
-
+    states = problem.create_plottable_states(states)
 
     ## Plotting ##
 
     # plt.ion()
 
     # figure_file_name = 'fig_runs-' + str(interactions) + "-" + agent.name() + ".png"
-    titlestring = agent.name()
-    # policy_plot2(problem, learned_policy, title=None, filename=titlestring)
-    # policy_heat_plot(problem, learned_policy, states)
+    # titlestring = agent.name()
+    policy_plot2(problem, learned_policy, title=None, filename=None)
+    policy_heat_plot(problem, learned_policy, states)
     # pickle_file_name = titlestring + '_' + time.strftime("%H%M%S") + '.p'
     # pickle.dump((payouts, moves, states, problem, agent), open(pickle_file_name, "wb"))
 

286a3c681b7143aad58076f1e1089e83bd7cc89f
Simon Wölzmüller <ga35voz@mytum.de>
2016-08-27 17:39:41 +0200

    mountain car debug

--- morlbench/scratchpad/morl_testbed_mountaincar.py
+++ morlbench/scratchpad/morl_testbed_mountaincar.py
@@ -17,24 +17,24 @@ if __name__ == '__main__':
         return mean_cont
 
     # create Problem
-    problem = MountainCar(acc_fac=0.008, cf=0.0025)
+    problem = MountainCar(acc_fac=0.01, cf=0.0025)
     # create an initialize randomly a weight vector
     scalarization_weights = [1.0, 0.0, 0.0]
     # tau is for chebyshev agent
-    tau = 4.0
+    tau = 1.0
     # ref point is used for Hypervolume calculation
-    ref = [-1.0, ]*problem.reward_dimension
+    ref = [-10.0, ]*problem.reward_dimension
     # learning rate
-    alfacheb = 0.3
+    alfacheb = 0.1
     # Propability of epsilon greedy selection
     eps = 0.9
     # should we show total acceleration count or just trend:
-    show_trend = False
+    show_trend = True
 
     # create one agent using chebyshev scalarization method
     chebyagent = MORLScalarizingAgent(problem, epsilon=eps, alpha=alfacheb, scalarization_weights=scalarization_weights,
-                                      ref_point=ref, tau=tau, function='chebishev', gamma=1.0)
-    hvbagent = MORLHVBAgent(problem, alfacheb, eps, ref, [1.0, 0.0])
+                                      ref_point=ref, tau=tau, gamma=0.9)
+    # hvbagent = MORLHVBAgent(problem, alfacheb, eps, ref, [0.0, 0.0])
 
     # both agents interact (times):
     interactions = 20
@@ -46,6 +46,7 @@ if __name__ == '__main__':
     #, moves, states = morl_interact_multiple_average_episodic(chebyagent, problem, 10, 500)
 
     # time = problem.time_token
+    velocity = problem.get_velocities(states)
     states = problem.create_plottable_states(states)
     plot_hypervolume([chebyagent], problem)
     forward_acc = []
@@ -73,13 +74,8 @@ if __name__ == '__main__':
     #         state_frequency[st] += mov.count(st)
     #
     f, axarr = plt.subplots(2, sharex=True)
-    velocity = [0]
-    for i in xrange(len(states[-1])-1):
-        velocity.append(states[-1][i+1] - states[-1][i])
-    for t in xrange(abs(len(states[-1])-len(velocity))):
-        velocity.append(velocity[t-1])
-    xv = np.arange(0, len(velocity))
-    axarr[0].plot(xv, velocity, 'y', label='velocity')
+    xv = np.arange(0, len(velocity[-1]))
+    axarr[0].plot(xv, velocity[-1], 'y', label='velocity')
     x = np.arange(0, len(states[-1]))
     axarr[1].plot(x, states[-1], 'b', label="states")
     y = np.zeros(len(states[-1]))
@@ -87,9 +83,9 @@ if __name__ == '__main__':
     left_front = np.zeros(len(states[-1]))
     y[:] = -0.5
     goal[:] = problem._goalxState
-    left_front[:] = -1.5
+    left_front[:] = -1.2
     plt.plot(x, y, 'm--', label='Minimum')
-    plt.axis([-1, 1.1*len(states[-1]), -1.55, 0.6])
+    plt.axis([-1, 1.1*len(states[-1]), -1.25, 0.6])
     plt.plot(x, goal, 'g--', label='goal')
     plt.plot(x, left_front, 'r--', label='left_front')
     axarr[0].legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)

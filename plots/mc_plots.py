6c33ade03f79da065f56e8c37030bc384c4db69e
Simon Wölzmüller <ga35voz@mytum.de>
2016-08-29 09:39:01 +0200

    plots for mountain car and puddleworld

---
+++ plots/mc_plots.py
@@ -0,0 +1,98 @@
+from morlbench.morl_problems import MountainCarTime, Deepsea, MORLGridworld, MountainCar
+from morlbench.morl_agents import MORLScalarizingAgent, MORLHVBAgent
+from morlbench.plotting_stuff import plot_hypervolume
+from morlbench.experiment_helpers import morl_interact_multiple_episodic, morl_interact_multiple_average_episodic
+
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import collections
+
+if __name__ == '__main__':
+    random.seed(43)
+    np.random.seed(43)
+    # we need this to show if accelerations have changed
+
+    def mean_continued(data):
+        mean_cont = [data[0]]
+        for ind in xrange(1, len(data)):
+            mean_cont.append((data[ind]+mean_cont[len(mean_cont)-1])/2.0)
+        return mean_cont
+
+    # create Problem
+    problem = MountainCar(acc_fac=0.0087, cf=0.0030)
+    # create an initialize randomly a weight vector
+    scalarization_weights = [0.0, 1.0, 0.0]
+    # tau is for chebyshev agent
+    tau = 1.0
+    # ref point is used for Hypervolume calculation
+    ref = [-10.0, ]*problem.reward_dimension
+    # learning rate
+    alfacheb = 0.4
+    # Propability of epsilon greedy selection
+    eps = 0.9
+    # should we show total acceleration count or just trend:
+    show_trend = True
+
+    # create one agent using chebyshev scalarization method
+    chebyagent = MORLScalarizingAgent(problem, epsilon=eps, alpha=alfacheb, scalarization_weights=scalarization_weights,
+                                      ref_point=ref, tau=tau, gamma=0.9)
+    # hvbagent = MORLHVBAgent(problem, alfacheb, eps, ref, [0.0, 0.0])
+
+    # both agents interact (times):
+    interactions = 200
+    #
+    payouts, moves, states = morl_interact_multiple_episodic(chebyagent, problem, interactions,
+                                                                 max_episode_length=300, discounted_eps=False)
+    # print("TEST(cheby): interactions made: \nP: "+str(payouts[:])+",\n M: " + str(moves[:]) + ",\n S: " +
+    #       str(states[:]) + '\n')
+    #, moves, states = morl_interact_multiple_average_episodic(chebyagent, problem, 10, 500)
+
+    # time = problem.time_token
+    chebyagent._epsilon = 0.9
+    payouts, moves2, states = morl_interact_multiple_episodic(chebyagent, problem, 1, 300)
+    velocity = problem.get_velocities(states)
+    states = problem.create_plottable_states(states)
+    plot_hypervolume([chebyagent], problem)
+    forward_acc = []
+    backward_acc = []
+    nothin = []
+    for i in xrange(len(moves)):
+        counter = list(moves[i])
+        nothin.append(counter.count(0))
+        forward_acc.append(counter.count(1))
+        backward_acc.append(counter.count(2))
+    x = np.arange(len(nothin))
+    if show_trend:
+        nothin = mean_continued(nothin)
+        backward_acc = mean_continued(backward_acc)
+        forward_acc = mean_continued(forward_acc)
+    plt.plot(x, nothin, 'y', label='no_acceleration')
+    plt.plot(x, forward_acc, 'g', label='forward acceleration')
+    plt.plot(x, backward_acc, 'r', label='backward acceleration')
+    plt.xlabel('epoch')
+    plt.ylabel('count')
+    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
+    plt.savefig('count')
+    f, axarr = plt.subplots(2, sharex=True)
+    xv = np.arange(0, len(velocity[-1]))
+    axarr[0].plot(xv, velocity[-1], 'y', label='velocity')
+    x = np.arange(0, len(states[-1]))
+    axarr[1].plot(x, states[-1], 'b', label="states")
+    y = np.zeros(len(states[-1]))
+    goal = np.zeros(len(states[-1]))
+    left_front = np.zeros(len(states[-1]))
+    y[:] = -0.5
+    goal[:] = problem._goalxState
+    left_front[:] = -1.2
+    axarr[1].plot(x, y, 'm--', label='minimum(start state)')
+    axarr[1].axis([-1, 1.1*len(states[-1]), -1.25, 0.6])
+    axarr[1].plot(x, goal, 'g--', label='goal position')
+    axarr[1].plot(x, left_front, 'r--', label='left wall')
+    axarr[0].legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
+    axarr[1].legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
+    axarr[1].set_xlabel('step')
+    axarr[1].set_ylabel('x')
+    axarr[0].set_ylabel('v')
+    plt.subplots_adjust(hspace=0.42)
+    plt.show()
---
+++ plots/pw_plot.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Mar 03, 2016
+
+@author: Johannes Feldmaier <@tum.de>
+"""
+
+import logging as log
+import numpy as np
+import sys
+
+import cPickle as pickle
+
+#log.basicConfig(level=log.DEBUG)
+log.basicConfig(level=log.INFO)
+
+
+from morlbench.morl_problems import MOPuddleworldProblem
+from morlbench.morl_agents import MORLScalarizingAgent, QMorlAgent, PreScalarizedQMorlAgent, SARSALambdaMorlAgent,\
+    SARSAMorlAgent, MORLHVBAgent
+from morlbench.morl_policies import PolicyFromAgent, PolicyGridworld
+from morlbench.inverse_morl import InverseMORLIRL
+from morlbench.plot_heatmap import policy_plot2, transition_map, heatmap_matplot, policy_heat_plot
+from morlbench.dynamic_programming import MORLDynamicProgrammingPolicyEvaluation, MORLDynamicProgrammingInverse
+from morlbench.experiment_helpers import morl_interact_multiple_episodic, morl_interact_multiple_average_episodic
+from morlbench.plotting_stuff import plot_hypervolume
+
+
+import pickle
+import time
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+
+if __name__ == '__main__':
+
+    runs = 1
+
+    saved_weights = []
+    plt.ion()
+
+    for i in xrange(runs):
+
+        problem = MOPuddleworldProblem(size=20)
+        scalarization_weights = np.array([1.0, 0.0])
+        max_episode_l = 200
+
+        alfa = 0.1
+        tau = 1.0
+
+        interactions = 50
+
+        def eps():
+            for i in xrange(interactions*max_episode_l/1.5):
+                yield 0.99
+            yield 0.6
+
+        agent = MORLScalarizingAgent(problem, scalarization_weights, alpha=alfa, epsilon=eps, tau=tau, lmbda=1.0,
+                                  ref_point=[-1.0, -1.0])
+
+        payouts, moves, states = morl_interact_multiple_episodic(agent, problem, interactions=interactions, max_episode_length=max_episode_l)
+        agent.create_scalar_Q_table()
+        x = [w for w in xrange(problem._size)]
+        y = [d for d in xrange(problem._size)]
+        x, y = np.meshgrid(x, y)
+        z = np.array([max([agent.Qs[s, a] for a in xrange(problem.n_actions)]) for s in xrange(problem.n_states)])
+        z = z.reshape(problem._size, problem._size)
+
+        fig, ax = plt.subplots()
+        ax.imshow(z, interpolation='nearest')
+        # plt.colorbar()
+        plt.grid()
+        plt.show()
+        log.info('Average Payout: %s' % (str(payouts.mean(axis=0))))
+
+
+        learned_policy = PolicyFromAgent(problem, agent, mode='greedy')
+        ## Plotting ##
+        expName = "Exp1-"
+        for w in range(len(scalarization_weights)):
+            expName += str(scalarization_weights[w])+"-"
+        expName += "_run_"
+        fName1 = expName + str(i) + '_learned'
+        fName2 = expName + str(i) + '_retrieved'
+
+        policy_plot2(problem, learned_policy)
+
+        plot_hypervolume([agent], problem)
+
+

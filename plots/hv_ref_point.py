3f00fbb5f1fb4d88f515aa5d6a51321043220025
Simon Wölzmüller <ga35voz@mytum.de>
2016-09-07 13:27:00 +0200

    hvb agent ref point influence experiment

--- plots/hv_ref_point.py
+++ plots/hv_ref_point.py
@@ -1,4 +1,4 @@
-from morlbench.morl_problems import MORLResourceGatheringProblem, MountainCarTime, MORLGridworld, MORLBuridansAss1DProblem, \
+from morlbench.morl_problems import MORLResourceGatheringProblem, MountainCar, MORLGridworld, MORLBuridansAss1DProblem, \
         Deepsea, MOPuddleworldProblem
 from morlbench.morl_agents import MORLScalarizingAgent, MORLHVBAgent
 from morlbench.experiment_helpers import morl_interact_multiple_episodic
@@ -14,26 +14,28 @@ import logging as log
 if __name__ == '__main__':
     # create Problem
     problem = MORLGridworld()
+    random.seed(2)
+    np.random.seed(2)
     # learning rate
     alfacheb = 0.11
     eps = 0.9
-    ref_points = [[-0.1, -100.0, -0.1], [-100.0, -0.1, -0.1], [-0.1, -0.1, -100.0]]
+    ref_points = [[10.0, -1000.0, 10.0], [-1000.0, 10.0, 10.0], [10.0, 10.0,  -1000.0]]
     agents = []
     scalarization_weights = [0.0, 0.0]
-    interactions = 8000
+    interactions = 1000
     log.info('Started reference point experiment')
     payoutslist = []
     for ref_p in xrange(len(ref_points)):
         agents.append(MORLHVBAgent(problem, alfacheb, eps, ref_points[ref_p], scalarization_weights))
 
-        payouts, moves, states = morl_interact_multiple_episodic(agents[ref_p], problem, interactions)
+        payouts, moves, states = morl_interact_multiple_episodic(agents[ref_p], problem, interactions, max_episode_length=300)
         payoutslist.append(payouts)
         policy = PolicyFromAgent(problem, agents[ref_p], mode='greedy')
         # policy_heat_plot(problem, policy, states)
 
     plot_hypervolume(agents, problem, name='reference point')
-    print 'final average reward' + str(sum(payoutslist[0]))
-    print 'final average reward' + str(sum(payoutslist[1]))
-    print 'final average reward' + str(sum(payoutslist[2]))
+    print 'final average reward' + str(np.mean(payoutslist[0], axis=0))
+    print 'final average reward' + str(np.mean(payoutslist[1], axis=0))
+    print 'final average reward' + str(np.mean(payoutslist[2], axis=0))
 
 
